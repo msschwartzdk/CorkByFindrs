56fbe0baab64863ec906637113ef0118baff7c74
msschwartzdk <msschwartz.regstrup@gmail.com>
2025-10-07 16:02:29 +0200

    Remake after build issue. Warning regarding kapt. version!!!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,6 @@ android {
 }
 
 dependencies {
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
